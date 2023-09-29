7d454f1e1206a5e0ee788af3ee16f42c5742f68e
leandrolog <leandro.oscargn@gmail.com>
2023-09-29 18:57:58 -0300

    primeiro commit

---
+++ src/main/java/com/dw/techshopbe/TechshopBeApplication.java
@@ -0,0 +1,13 @@
+package com.dw.techshopbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechshopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechshopBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dw/techshopbe/TechshopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dw.techshopbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechshopBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
